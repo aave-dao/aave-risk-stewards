fba595ad008d9f639c39518230e7f9ac5f111e3e
Sagi Shoshan <sagi@chaoslabs.xyz>
2023-10-05 15:24:08 +0300

    Arbitrum V3 Cap Increase 20231005

---
+++ diffs/pre_arbitrum_caps_increase_20231005_post_arbitrum_caps_increase_20231005.md
@@ -0,0 +1,38 @@
+## Reserve changes
+
+### Reserve altered
+
+#### rETH ([0xEC70Dcb4A1EFa46b8F2D97C310C9c4790ba5ffA8](https://arbiscan.io/address/0xEC70Dcb4A1EFa46b8F2D97C310C9c4790ba5ffA8))
+
+| description | value before | value after |
+| --- | --- | --- |
+| supplyCap | 1,700 rETH | 2,600 rETH |
+
+
+#### LINK ([0xf97f4df75117a78c1A5a0DBb814Af92458539FB4](https://arbiscan.io/address/0xf97f4df75117a78c1A5a0DBb814Af92458539FB4))
+
+| description | value before | value after |
+| --- | --- | --- |
+| supplyCap | 1,300,000 LINK | 1,450,000 LINK |
+
+
+## Raw diff
+
+```json
+{
+  "reserves": {
+    "0xEC70Dcb4A1EFa46b8F2D97C310C9c4790ba5ffA8": {
+      "supplyCap": {
+        "from": 1700,
+        "to": 2600
+      }
+    },
+    "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4": {
+      "supplyCap": {
+        "from": 1300000,
+        "to": 1450000
+      }
+    }
+  }
+}
+```
---
+++ src/ArbitrumCapsIncrease_20231005.s.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3ArbitrumAssets} from 'aave-address-book/AaveV3Arbitrum.sol';
+import {IAaveV3ConfigEngine} from 'aave-helpers/v3-config-engine/IAaveV3ConfigEngine.sol';
+import {EngineFlags} from 'aave-helpers/v3-config-engine/EngineFlags.sol';
+import {CapsPlusRiskStewardArbitrum} from '../scripts/CapsPlusRiskStewardArbitrum.s.sol';
+
+/**
+ * @title Update Caps on Arbitrum V3
+ * @author @ChaosLabsInc
+ * - Discussion: https://governance.aave.com/t/arfc-chaos-labs-risk-stewards-increase-supply-and-borrow-caps-on-v3-metis-arbitrum-10-03-2023/15038
+ */
+contract ArbitrumCapsIncrease_20231005 is CapsPlusRiskStewardArbitrum {
+  /**
+   * @return string name identifier used for the diff
+   */
+  function name() internal pure override returns (string memory) {
+    return 'arbitrum_caps_increase_20231005';
+  }
+
+  /**
+   * @return IAaveV3ConfigEngine.CapsUpdate[] capUpdates to be performed
+   */
+  function capsUpdates() internal pure override returns (IAaveV3ConfigEngine.CapsUpdate[] memory) {
+    IAaveV3ConfigEngine.CapsUpdate[] memory capUpdates = new IAaveV3ConfigEngine.CapsUpdate[](2);
+
+    capUpdates[0] = IAaveV3ConfigEngine.CapsUpdate(
+      AaveV3ArbitrumAssets.rETH_UNDERLYING,
+      2_600,
+      EngineFlags.KEEP_CURRENT
+    );
+
+    capUpdates[1] = IAaveV3ConfigEngine.CapsUpdate(
+      AaveV3ArbitrumAssets.LINK_UNDERLYING,
+      1_450_000,
+      EngineFlags.KEEP_CURRENT
+    );
+
+    return capUpdates;
+  }
+}
