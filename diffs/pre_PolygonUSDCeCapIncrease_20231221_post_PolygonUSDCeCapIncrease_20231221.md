2c9f8ed4bab4fe087a694d7fcfbf12f60c630984
yonikesel <yonikesel@chaoslabs.xyz>
2023-12-21 21:09:29 +0200

    Increase Caps and Execute AIP-414

---
+++ diffs/pre_PolygonUSDCeCapIncrease_20231221_post_PolygonUSDCeCapIncrease_20231221.md
@@ -0,0 +1,30 @@
+## Reserve changes
+
+### Reserves altered
+
+#### USDC ([0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174](https://polygonscan.com/address/0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174))
+
+| description | value before | value after |
+| --- | --- | --- |
+| supplyCap | 40,000,000 USDC | 48,000,000 USDC |
+| borrowCap | 36,000,000 USDC | 43,500,000 USDC |
+
+
+## Raw diff
+
+```json
+{
+  "reserves": {
+    "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174": {
+      "borrowCap": {
+        "from": 36000000,
+        "to": 43500000
+      },
+      "supplyCap": {
+        "from": 40000000,
+        "to": 48000000
+      }
+    }
+  }
+}
+```
---
+++ src/PolygonUSDCeCapIncrease_20231221.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3PolygonAssets} from 'aave-address-book/AaveV3Polygon.sol';
+import {IAaveV3ConfigEngine} from 'aave-helpers/v3-config-engine/IAaveV3ConfigEngine.sol';
+import {EngineFlags} from 'aave-helpers/v3-config-engine/EngineFlags.sol';
+import {CapsPlusRiskStewardPolygon} from '../scripts/CapsPlusRiskStewardPolygon.s.sol';
+
+/**
+ * @title Increase USDC.e Cap on Polygon V3, and execute AIP-414
+ * @author @ChaosLabsInc
+ * - Discussion:
+ */
+contract PolygonUSDCeCapIncrease_20231221 is CapsPlusRiskStewardPolygon {
+  /**
+   * @return string name identifier used for the diff
+   */
+  function name() internal pure override returns (string memory) {
+    return 'PolygonUSDCeCapIncrease_20231221';
+  }
+  
+
+  /**
+   * @return IAaveV3ConfigEngine.CapsUpdate[] capUpdates to be performed
+   */
+  function capsUpdates() internal pure override returns (IAaveV3ConfigEngine.CapsUpdate[] memory) {
+    IAaveV3ConfigEngine.CapsUpdate[] memory capUpdates = new IAaveV3ConfigEngine.CapsUpdate[](1);
+
+    capUpdates[0] = IAaveV3ConfigEngine.CapsUpdate(
+      AaveV3PolygonAssets.USDC_UNDERLYING,
+      48_000_000,
+      43_500_000
+    );
+
+    return capUpdates;
+  }
+}
