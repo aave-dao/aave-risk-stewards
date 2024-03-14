e8fa33c1795995eee5b90adcd5c1129597698737
Eyal Ovadya <eyal.ovadya7@gmail.com>
2024-03-14 19:14:51 +0200

    Mainnet Caps Increase 20240313

---
+++ src/MainnetCapsIncrease_20240313.s.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IAaveV3ConfigEngine} from 'aave-helpers/v3-config-engine/IAaveV3ConfigEngine.sol';
+import {EngineFlags} from 'aave-helpers/v3-config-engine/EngineFlags.sol';
+import {CapsPlusRiskStewardMainnet} from '../scripts/CapsPlusRiskStewardMainnet.s.sol';
+import {AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+
+/**
+ * @title Increase Caps on Ethereum V3
+ * @author @ChaosLabsInc
+ */
+contract MainnetCapsIncrease_20240313 is CapsPlusRiskStewardMainnet {
+  /**
+   * @return string name identifier used for the diff
+   */
+  function name() internal pure override returns (string memory) {
+    return 'mainnetCapsIncrease_20240313';
+  }
+
+  /**
+   * @return IAaveV3ConfigEngine.CapsUpdate[] capUpdates to be performed
+   */
+  function capsUpdates() internal pure override returns (IAaveV3ConfigEngine.CapsUpdate[] memory) {
+    IAaveV3ConfigEngine.CapsUpdate[] memory capUpdates = new IAaveV3ConfigEngine.CapsUpdate[](2);
+
+    capUpdates[0] = IAaveV3ConfigEngine.CapsUpdate(
+      AaveV3EthereumAssets.PYUSD_UNDERLYING,
+      EngineFlags.KEEP_CURRENT,
+      30_000_000
+    );
+
+    capUpdates[1] = IAaveV3ConfigEngine.CapsUpdate(
+      AaveV3EthereumAssets.LDO_UNDERLYING,
+      8_000_000,
+      EngineFlags.KEEP_CURRENT
+    );
+
+    return capUpdates;
+  }
+}
