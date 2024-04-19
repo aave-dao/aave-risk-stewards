9f8763a0618af82be646e435e3e7f01f22f0fcfc
Eyal Ovadya <eyal.ovadya7@gmail.com>
2024-04-19 21:14:51 +0300

    Update Caps for weETH Ethereum V3

---
+++ src/MainnetWeETHCapsIncrease_20240419.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {IAaveV3ConfigEngine} from 'aave-helpers/v3-config-engine/IAaveV3ConfigEngine.sol';
+import {EngineFlags} from 'aave-helpers/v3-config-engine/EngineFlags.sol';
+import {CapsPlusRiskStewardMainnet} from '../scripts/CapsPlusRiskStewardMainnet.s.sol';
+
+/**
+ * @title Update Caps for weETH Ethereum V3
+ * @author @ChaosLabsInc
+ * - Discussion: https://governance.aave.com/t/arfc-chaos-labs-risk-stewards-increase-supply-cap-for-weeth-on-v3-ethereum-04-14-2024/17363
+ */
+contract MainnetWeETHCapsIncrease_20240419 is CapsPlusRiskStewardMainnet {
+  /**
+   * @return string name identifier used for the diff
+   */
+  function name() internal pure override returns (string memory) {
+    return 'mainnetWeETHCapsIncrease_20240419';
+  }
+
+  /**
+   * @return IAaveV3ConfigEngine.CapsUpdate[] capUpdates to be performed
+   */
+  function capsUpdates() internal pure override returns (IAaveV3ConfigEngine.CapsUpdate[] memory) {
+    IAaveV3ConfigEngine.CapsUpdate[] memory capUpdates = new IAaveV3ConfigEngine.CapsUpdate[](1);
+
+    // weETH
+    capUpdates[0] = IAaveV3ConfigEngine.CapsUpdate(
+      0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee,
+      32_000,
+      1_600
+    );
+
+    return capUpdates;
+  }
+}
