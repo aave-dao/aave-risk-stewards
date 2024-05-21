17202079b2fc42e7f0dd91f5e812352626266f42
DanielYamin <dhyamin@gmail.com>
2024-05-21 15:18:16 +0300

    mainnet added

---
+++ diffs/pre_MainnetCapsUpdate_20240521_post_MainnetCapsUpdate_20240521.md
@@ -0,0 +1,31 @@
+## Reserve changes
+
+### Reserve altered
+
+#### BAL ([0xba100000625a3754423978a60c9317c58a424e3D](https://etherscan.io/address/0xba100000625a3754423978a60c9317c58a424e3d))
+
+| description | value before | value after |
+| --- | --- | --- |
+| supplyCap | 1,400,000 BAL | 2,100,000 BAL |
+| borrowCap | 244,200 BAL | 250,000 BAL |
+
+## Raw diff
+
+```json
+{
+  "reserves": {
+    "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32": {
+      "supplyCap": {
+        "from": 1400000,
+        "to": 2100000
+      }
+    },
+    "0x6c3ea9036406852006290770BEdFcAbA0e23A0e8": {
+      "borrowCap": {
+        "from": 244200,
+        "to": 250000
+      }
+    }
+  }
+}
+```
---
+++ src/MainnetCapsUpdate_20240521.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {IAaveV3ConfigEngine} from 'aave-helpers/v3-config-engine/IAaveV3ConfigEngine.sol';
+import {CapsPlusRiskStewardMainnet} from '../scripts/CapsPlusRiskStewardMainnet.s.sol';
+
+/**
+ * @title Increase Supply and Borrow Caps on Aave V3
+ * @author Chaos Labs
+ * - Discussion: https://governance.aave.com/t/arfc-chaos-labs-risk-stewards-increase-supply-and-borrow-caps-on-aave-v3-05-20-2024/17752
+ */
+contract MainnetCapsUpdate_20240521 is CapsPlusRiskStewardMainnet {
+  /**
+   * @return string name identifier used for the diff
+   */
+  function name() internal pure override returns (string memory) {
+    return 'MainnetCapsUpdate_20240521';
+  }
+
+  /**
+   * @return IAaveV3ConfigEngine.CapsUpdate[] capUpdates to be performed
+   */
+  function capsUpdates() internal pure override returns (IAaveV3ConfigEngine.CapsUpdate[] memory) {
+    IAaveV3ConfigEngine.CapsUpdate[] memory capUpdates = new IAaveV3ConfigEngine.CapsUpdate[](1);
+
+    capUpdates[0] = IAaveV3ConfigEngine.CapsUpdate(
+      AaveV3EthereumAssets.BAL_UNDERLYING,
+      2_100_000,
+      250_000
+    );
+    return capUpdates;
+  }
+}
